a44180509a85e8099d512268be957042aca96c8c
tahmed61 <tawsif29bikky@gmail.com>
2024-01-09 22:01:55 -0500

    commit sum of numbers in pom.xml

---
+++ src/main/java/Sumofnumbers.java
@@ -0,0 +1,12 @@
+
+public class Sumofnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5;
+int b=10;
+int sum=a+b;
+System.out.println(sum);
+	}
+
+}

98d8347d31b01ab1b80e477744278afb562339d2
tahmed61 <tawsif29bikky@gmail.com>
2024-01-11 21:42:47 -0500

    added multiply functionality

---
+++ src/main/java/Multiplyofnumbers.java
@@ -0,0 +1,14 @@
+
+public class Multiplyofnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=5;
+int c=2;
+int d=3;
+int multiply=a*b*c*d;
+System.out.println(multiply);
+	}
+
+}
